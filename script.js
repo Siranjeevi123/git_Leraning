9528b635090cb38416ac3af5a9984e8b7f3fc699
C Siranjeevi <venketsiranjeevi.752f@gmail.com>
2025-02-01 19:40:05 +0530

    Payment Update After Footer

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ console.log(button);
 const login = "Login Page Added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer = "Footer Added in our Website";
 console.log(footer);
+=======
+const Payment = "Integrated the Payment Gateway";
+console.log(Payment);
+>>>>>>> 2a22764 (Payment Added)
